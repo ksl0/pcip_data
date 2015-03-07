74c6c191cd3f45a8f7246fb7bdee7b84852e7a0a
Katie Li <katiesli16@gmail.com>
2015-03-07 02:10:52 -0800

    fixing up some variables

--- scripts/geocode.py
+++ scripts/geocode.py
@@ -16,9 +16,12 @@ import math
 # the debugger
 #ipdb.set_trace()
 df = pd.read_csv('../csv/retrofit.csv', header=0)
+#df = df[2:3]
 base_url ='https://maps.googleapis.com/maps/api/geocode/json?address={0}&bounds={2}&key={1}'
 key = keys.API_KEY
 city = "claremont"
+newDF = pd.DataFrame();
+testing = {}
 
 # from http://www.maptechnica.com/us-city-boundary-map/city/Claremont/state/CA/cityid/0613756
 def claremontCityLimits(): 
@@ -43,19 +46,21 @@ def format_address(base_url, address, bound):
 bad_urls = {}
 def get_data(url): 
     data = req.get(url).json()
+    testing.update(data)
     dictionary = {}
     print data
     if (len(data['results']) == 0): 
        adrs = np.nan
        lat = np.nan
        lng = np.nan
+       place_id = np.nan
        bad_urls["url"] = [url]
     else: 
       adrs = data['results'][0]['formatted_address'].encode('ascii', 'ignore')
       lat = data['results'][0]['geometry']['location']['lat'] 
       lng = data['results'][0]['geometry']['location']['lng'] 
-      
-    dictionary = {'address_f': [adrs], 'lat': [lat], 'lng': [lng]} 
+      place_id = data['results'][0]['place_id'].encode()
+    dictionary = {'place_id': [place_id], 'address_f': [adrs], 'lat': [lat], 'lng': [lng]} 
     return dictionary 
 
 #needs to have an address column in df
@@ -66,6 +71,7 @@ def populate_claremont_address(dataFrame, bounds):
     dct['lat'] = [] 
     dct['lng'] = []
     dct['address_f'] = []
+    dct['place_id'] = []
     for i in dataFrame.Address: 
         print i
         time.sleep(0.2) #make sure not to exceed API limits
@@ -73,12 +79,14 @@ def populate_claremont_address(dataFrame, bounds):
            dct['lat'].append(np.nan) 
            dct['lng'].append(np.nan) 
            dct['address_f'].append(np.nan)
+           dct['place_id'].append(np.nan)
         else:
     	   url = format_address(base_url, i, bounds)
            nDict = get_data(url)
            dct['lat'].append(nDict["lat"][0]) 
            dct['lng'].append(nDict["lng"][0])
            dct['address_f'].append(nDict["address_f"][0])
+           dct['place_id'].append(nDict["place_id"][0])
     return dct 
            
 
